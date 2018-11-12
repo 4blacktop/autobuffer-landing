a257174b12510529c9ed06423f0c20b31d6e14cd
4blacktop <4blacktop@gmail.com>
2018-11-12 20:00:50 +1000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="ru"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
+	<title>Автобаферы (Авто баферы) Power Guard + Жидкое стекло в подарок </title>
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width">
+
+	<!-- CSS -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500i,700,700i&amp;subset=cyrillic" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="files/style.css">
+
+	<!-- JS -->
+	<script type="text/javascript" async="" src="files/watch.js"></script>
+	<script src="files/jquery-1.11.1.min.js" type="text/javascript"></script>
+	<script type="text/javascript" src="files/validation.js"></script>
+	<script type="text/javascript" src="files/ab.min.js"></script>
+	<script type="text/javascript" src="files/timer.js"></script>
+  	<script type="text/javascript" src="files/script.js"></script>
+</head>
+<body>
+
+
+
+
+
+<div class="wrap">
+   <section class="main">
+      <div class="container">
+         <header class="cf">
+            <a class="logo"><img src="files/logo.png" alt="Автобаферы Power Guard" title="Автобаферы Power Guard"></a>
+            <div class="address">
+               <p>Отличная скидка!<br>-50% только сегодня</p>
+            </div>
+            <div class="right-info">
+               <a href="#form" class="call">Обратный звонок</a>
+            </div>
+         </header>
+         <div class="main-wrap cf">
+            <h1>Оригинальные немецкие автобаферы Power Guard</h1>
+            <h2><span class="underline">защищают ходовую</span> и могут сэкономить Вам больше <span class="blue">40 600 руб!</span></h2>
+            <div class="price">
+               <p>Цена: <span class="displayPrevPriceShortCyr"><i class="price_land_s2">3980</i> <i class="price_land_curr">руб.</i></span></p>
+               <div class="displayCurrentPriceShortCyr"><i class="price_land_s1">1990</i> <i class="price_land_curr">руб.</i></div>
+            </div>
+            <ul>
+               <li>
+                  <h3>Увеличьте дорожный просвет на 3 см</h3>
+                  <p>Защитите авто от ударов о кочки и выбоины!</p>
+               </li>
+               <li>
+                  <h3>Позаботьтесь о подвеске и амортизаторах</h3>
+                  <p>Они могут прослужат до 3 раза дольше!</p>
+               </li>
+               <li>
+                  <h3>Комфортное вождение</h3>
+                  <p>Уменьшается крен при маневрах</p>
+               </li>
+               <li>
+                  <h3>Обращайтесь в автосервис до 3 раз реже!</h3>
+                  <p>Сэкономьте время и деньги на ремонт!</p>
+               </li>
+               <li>
+                  <h3>Даже в экстремальных условиях</h3>
+                  <p>Защита авто на бездорожье</p>
+               </li>
+            </ul>
+            <a href="#form" class="btn">Закажите сейчас</a>
+         </div>
+      </div>
+            <img src="files/main-car.png" alt="Автобаферы Power Guard" title="Автобаферы Power Guard" class="main-car">
+         </section>
+
+      <section class="need">
+      <div class="container">
+         <h2 class="title">Автобаферы Power Guard –</h2>
+         <h3 class="sub-title">необходимы в наших условиях!</h3>
+         <div class="need-wrap cf">
+            <div class="need-item">
+               <img src="files/need-1.jpg" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+               <h3>ямы</h3>
+            </div>
+            <div class="need-item">
+               <h3>Выбоины</h3>
+               <img src="files/need-2.jpg" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+            </div>
+            <div class="need-item">
+               <img src="files/need-3.jpg" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+               <h3>Грунтовка</h3>
+            </div>
+            <div class="need-item">
+               <h3>Бездорожье</h3>
+               <img src="files/need-4.jpg" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+            </div>
+         </div>
+         <h3 class="result-title">как Результат:</h3>
+         <div class="result-wrap cf">
+            <div class="result-item i1">
+               <img src="files/result-1.jpg" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+               <h4>Сломанная ходовая</h4>
+               <p>Большие затраты на ремонт!</p>
+            </div>
+            <div class="result-item i2">
+               <img src="files/result-2.jpg" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+               <h4>дтп</h4>
+               <p>Серьезные траты на восстановление!</p>
+            </div>
+            <div class="result-item i3">
+               <img src="files/result-3.jpg" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+               <h4>большие убытки</h4>
+               <p>Вы рискуете потерять<br>авто навсегда!</p>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section class="problem">
+      <div class="container">
+         <h2 class="title">Автобаферы Power Guard избавят вас от этих проблем!</h2>
+         <h3 class="sub-title">Реальная экономия денежных средств!</h3>
+         <div class="problem-wrap cf">
+            <div class="left">
+               <h3>Без автобафера</h3>
+               <div class="info">
+                  <p>Расходы на ремонт ходовой </p>
+                  <h4>До 30700-40600 р </h4>
+                  <h5>на каждые 100 тыс. км.</h5>
+               </div>
+            </div>
+            <div class="right">
+               <h3>С автобаферами Power Guard</h3>
+               <div class="info">
+                  <p>Расходы на ремонт ходовой</p>
+                  <h4>От 0-2000р. </h4>
+                  <h5>на каждые 100 тыс. км.</h5>
+               </div>
+            </div>
+         </div>
+         <div class="problem-box">
+            <p>Потратьте свои финансы на что-нибудь действительно приятное, а не на ремонт ходовки своего авто!<br>
+               С автобаферами <span>Power Guard</span> это так просто!
+            </p>
+            <a href="#form" class="btn">Закажите сейчас</a>
+         </div>
+                     <div class="warning-wrap cf">
+            <div class="warning-item">
+               <div class="img">
+                  <img src="files/warning-1.png" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+               </div>
+               <h3>Осторожно! Подделки!</h3>
+               <p>Подлинные автобаферы Power Guard пр-ва Германии защищают ходовую автомобиля и экономят ваши деньги.</p>
+               <p>Мы не отвечаем за подделки, которых сейчас очень много на рынке.</p>
+               <p>Совершая заказ на этом сайте, вы получите оригинальные немецкие автобаферы Power Guard, которые соответствуют всем заявленным характеристикам.</p>
+            </div>
+            <div class="warning-item">
+               <div class="img">
+                  <img src="files/warning-2.png" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+               </div>
+               <h3>Европейская гарантия качества!</h3>
+               <p>На автобаферы Power Guard распространяется гарантия в полном соответствии с правилами Евросоюза.</p>
+               <p>Мы гарантируем, что автобаферы Power Guard будут в полной мере выполнять все свои функции 5 лет или<br> 100 тыс. км.</p>
+               <p>Иначе мы вернем вам деньги без разговоров! </p>
+            </div>
+         </div>
+                  </div>
+   </section>
+
+   <section class="shema">
+      <div class="container">
+         <div class="risk">
+            <h2 class="title">Покупайте без риска!</h2>
+            <div class="risk-wrap cf">
+               <div class="risk-item">
+                  <img src="files/risk-1.png" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+                  <h3>Заказывайте сейчас</h3>
+               </div>
+               <div class="risk-item">
+                  <img src="files/risk-2.png" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+                  <h3>Получите товар на почте</h3>
+               </div>
+               <div class="risk-item">
+                  <img src="files/risk-3.png" alt="Автобаферы Power Guard" title="Автобаферы Power Guard">
+                  <h3>Оплата после получения!</h3>
+               </div>
+            </div>
+         </div>
+         <div class="testimonials">
+            <h2 class="title">Отзывы покупателей</h2>
+         </div>
+         <div class="owl-carousel owl-theme owl-loaded">
+            
+            
+            
+         <div class="owl-stage-outer1"><div class="owl-stage" style="transform: translate3d(-969.999px, 0px, 0px); transition: 0s; width: 2910px;"><div class="owl-item cloned" style="width: 313.333px; margin-right: 10px;"><div class="item">
+               <div class="testimonial-item">
+                  <div class="name">Артем</div>
+                  <div class="video-wrap">
+                     
+                     <div class="youtube" id="6J_vxFhR3q4" style="background-image: url("http://i.ytimg.com/vi/6J_vxFhR3q4/hqdefault.jpg");"><div class="play"></div></div>
+                  </div>
+                  <div class="city">г. Новокузнецк</div>
+               </div>
+            </div></div><div class="owl-item cloned" style="width: 313.333px; margin-right: 10px;"><div class="item">
+               <div class="testimonial-item">
+                  <div class="name">Григорий</div>
+                  <div class="video-wrap">
+                     
+                     <div class="youtube" id="xj5P2xsUKoQ" style="background-image: url("http://i.ytimg.com/vi/xj5P2xsUKoQ/hqdefault.jpg");"><div class="play"></div></div>
+                  </div>
+                  <div class="city">г. Москва</div>
+               </div>
+            </div></div><div class="owl-item cloned" style="width: 313.333px; margin-right: 10px;"><div class="item">
+               <div class="testimonial-item">
+                  <div class="name">Сергей</div>
+                  <div class="video-wrap">
+                     
+                     <div class="youtube" id="Sc22WMa-eTE" style="background-image: url("http://i.ytimg.com/vi/Sc22WMa-eTE/hqdefault.jpg");"><div class="play"></div></div>
+                  </div>
+                  <div class="city">г. Тверь</div>
+               </div>
+            </div></div><div class="owl-item active" style="width: 313.333px; margin-right: 10px;"><div class="item">
+               <div class="testimonial-item">
+                  <div class="name">Артем</div>
+                  <div class="video-wrap">
+                     
+                     <div class="youtube" id="6J_vxFhR3q4" style="background-image: url("http://i.ytimg.com/vi/6J_vxFhR3q4/hqdefault.jpg");"><div class="play"></div></div>
+                  </div>
+                  <div class="city">г. Новокузнецк</div>
+               </div>
+            </div></div><div class="owl-item active" style="width: 313.333px; margin-right: 10px;"><div class="item">
+               <div class="testimonial-item">
+                  <div class="name">Григорий</div>
+                  <div class="video-wrap">
+                     
+                     <div class="youtube" id="xj5P2xsUKoQ" style="background-image: url("http://i.ytimg.com/vi/xj5P2xsUKoQ/hqdefault.jpg");"><div class="play"></div></div>
+                  </div>
+                  <div class="city">г. Москва</div>
+               </div>
+            </div></div><div class="owl-item active" style="width: 313.333px; margin-right: 10px;"><div class="item">
+               <div class="testimonial-item">
+                  <div class="name">Сергей</div>
+                  <div class="video-wrap">
+                     
+                     <div class="youtube" id="Sc22WMa-eTE" style="background-image: url("http://i.ytimg.com/vi/Sc22WMa-eTE/hqdefault.jpg");"><div class="play"></div></div>
+                  </div>
+                  <div class="city">г. Тверь</div>
+               </div>
+            </div></div><div class="owl-item cloned active" style="width: 313.333px; margin-right: 10px;"><div class="item">
+               <div class="testimonial-item">
+                  <div class="name">Артем</div>
+                  <div class="video-wrap">
+                     
+                     <div class="youtube" id="6J_vxFhR3q4" style="background-image: url("http://i.ytimg.com/vi/6J_vxFhR3q4/hqdefault.jpg");"><div class="play"></div></div>
+                  </div>
+                  <div class="city">г. Новокузнецк</div>
+               </div>
+            </div></div><div class="owl-item cloned" style="width: 313.333px; margin-right: 10px;"><div class="item">
+               <div class="testimonial-item">
+                  <div class="name">Григорий</div>
+                  <div class="video-wrap">
+                     
+                     <div class="youtube" id="xj5P2xsUKoQ" style="background-image: url("http://i.ytimg.com/vi/xj5P2xsUKoQ/hqdefault.jpg");"><div class="play"></div></div>
+                  </div>
+                  <div class="city">г. Москва</div>
+               </div>
+            </div></div><div class="owl-item cloned" style="width: 313.333px; margin-right: 10px;"><div class="item">
+               <div class="testimonial-item">
+                  <div class="name">Сергей</div>
+                  <div class="video-wrap">
+                     
+                     <div class="youtube" id="Sc22WMa-eTE" style="background-image: url("http://i.ytimg.com/vi/Sc22WMa-eTE/hqdefault.jpg");"><div class="play"></div></div>
+                  </div>
+                  <div class="city">г. Тверь</div>
+               </div>
+            </div></div></div></div><div class="owl-controls"><div class="owl-nav"><div class="owl-prev" style="">prev</div><div class="owl-next" style="">next</div></div><div class="owl-dots" style=""><div class="owl-dot active"><span></span></div></div></div></div>
+      </div>
+   </section>
+
+   <section class="share">
+      <div class="container">
+         <div class="share-wrap cf">
+            <div class="left">
+               <h2>Распродажа остатков со склада!</h2>
+               <h3>Оригинальные немецкие автобаферы Power Guard</h3>
+               <div><img src="files/present.png" alt="" class="share-img"></div>
+               <div class="price">
+                  <p>Цена: <span class="displayPrevPriceShortCyr"><i class="price_land_s2">3980</i> <i class="price_land_curr">руб.</i></span></p>
+                  <div class="displayCurrentPriceShortCyr"><i class="price_land_s1">1990</i> <i class="price_land_curr">руб.</i></div>
+               </div>
+               <img src="files/share-img.png" alt="Авто баферы Power Guard" title="Авто баферы Power Guard" class="share-img">
+               <div class="easy-wrap cf">
+                  <div class="easy-item">
+                     <div class="img">
+                        <img src="files/share-1.png" alt="Авто баферы Power Guard" title="Авто баферы Power Guard">
+                     </div>
+                     <p>Простая<br> установка</p>
+                  </div>
+                  <div class="easy-item">
+                     <div class="img">
+                        <img src="files/share-2.png" alt="Авто баферы Power Guard" title="Авто баферы Power Guard">
+                     </div>
+                     <p>Качество<br> Германия</p>
+                  </div>
+                  <div class="easy-item">
+                     <div class="img">
+                        <img src="files/share-3.png" alt="Авто баферы Power Guard" title="Авто баферы Power Guard">
+                     </div>
+                     <p>Платите при<br> получении</p>
+                  </div>
+               </div>
+            </div>
+            <div class="right">
+               <div class="in">
+                  <p>На складе: <span class="lastpack">39</span> <img src="files/pulse.png" alt="Авто баферы Power Guard" title="Авто баферы Power Guard" style="opacity: 0.905082;"> штук!</p>
+               </div>
+				
+				<iframe id="form" src="http://c.tvkw.ru/#TODO-insert-ID" width="468" height="440" align="left" frameborder="0">
+				Ваш браузер не поддерживает плавающие фреймы!
+				</iframe>
+				
+            </div>
+         </div>
+      </div>
+   </section>
+   <footer class="cf">
+      <div class="container">
+         <a class="logo"><img src="files/logo-footer.png" alt="Автобаферы Power Guard" title="Автобаферы Power Guard"></a>
+         <div class="address">
+            <p>
+            </p>
+         </div>
+         <div class="right-info">
+         </div>
+      </div>
+   </footer>
+</div>
+<div class="center">
+   <div class="copyright"><img src="files/rekv.png" style="display:block;margin:0 auto">
+<img src="files/logo.jpg" style="display:block;margin:0 auto"><br><a href="privacy.php" target="_blank">Политика конфиденциальности</a></div>
+</div>
+
+<link rel="stylesheet" href="files/style1.css">
+<script src="files/script1.js"></script>
+
+</body></html>
